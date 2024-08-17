81e315e972b0156923087e4953d7475fa68e9839
techsumorg <techsumorg@users.noreply.github.com>
2024-08-17 19:08:58 +0000

    update: generated bucket and output Triggered by 0773b9a8916a89c6b9023c298e314217cfb608b3

--- metadata.ts
+++ metadata.ts
@@ -1,5 +1,5 @@
 /**
- * This is auto generated metadata file, generated at: Sat Aug 17 2024 19:03:26 GMT+0000 (Coordinated Universal Time)
+ * This is auto generated metadata file, generated at: Sat Aug 17 2024 19:08:46 GMT+0000 (Coordinated Universal Time)
  * Prevent making any changes here
  */
 
@@ -27,5 +27,5 @@ export interface Metadata {
 }
 
 export const metadata = JSON.parse(
-  '{"github":{"repository":"techsumorg/blogger-pwa","branch":"main"},"pwa":{"oneSignalEnabled":false,"oneSignalSDK":"https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js","oneSignalConfig":{"appId":"********-****-****-****-************","allowLocalhostAsSecureOrigin":true},"logs":true,"serviceWorker":{"source":"/app/serviceworker.js","scope":"/"}},"build":{"hash":"5l5h_-qK05-3deM0oNOmf"}}',
+  '{"github":{"repository":"techsumorg/blogger-pwa","branch":"main"},"pwa":{"oneSignalEnabled":false,"oneSignalSDK":"https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js","oneSignalConfig":{"appId":"********-****-****-****-************","allowLocalhostAsSecureOrigin":true},"logs":true,"serviceWorker":{"source":"/app/serviceworker.js","scope":"/"}},"build":{"hash":"E4NNRNiG4Fk7hWe93JjPM"}}',
 ) as Metadata;
