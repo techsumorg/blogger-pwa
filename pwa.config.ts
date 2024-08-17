b2d2ced1a73f52215e99673c8a75b32483f2d822
techsumorg <techsum.org@gmail.com>
2024-08-17 22:36:28 +0700

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -3,33 +3,19 @@ import type { Config } from './types';
 export default {
   version: '1.0',
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'TECHSum',
+  shortName: 'TECHSum',
+  description: 'TECHSum - Tổng hợp tin tức công nghệ hàng đầu, cập nhật nhanh chóng',
   direction: 'auto',
-  language: 'en-US',
+  language: 'vi-VN',
   backgroundColor: '#fff',
-  themeColor: '#fff',
+  themeColor: '#4f6f52',
   display: 'standalone',
   orientation: 'any',
   scope: '/',
-  startUrl: '/?utm_source=homescreen',
+  startUrl: '/',
   appleStatusBarStyle: 'black-translucent',
   preferRelatedApplications: false,
-  shortcuts: [
-    {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
-    },
-    {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
-    },
-  ],
   pwa: {
     logs: true,
     // OneSignal is not available if you are not using cloudflare workers
@@ -40,5 +26,5 @@ export default {
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'https://www.techsum.org',
 } satisfies Config;
